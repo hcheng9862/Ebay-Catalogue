17d31585cc4b5fb0f6b5c1fd682fb754c35160ce
hcheng9862 <hcheng9862@gmail.com>
2018-10-18 14:40:39 -0500

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,32 +1,32 @@
-
-
-console.log("I'm here");
-
-var bd = document.body;
-bd.style.backgroundColor = "White";
-
-
-
-var headerElement=document.createElement('h1')
-headerElement.textContent="404 PAGE NOT FOUND";
-bd.appendChild(headerElement);
-headerElement.style.color = "rgb(0, 255, 75)";
-
-var grabbed = document.getElementById("a");
-grabbed.style.color = "rgb(255, 0, 255)";
-grabbed.style.color="rgb(0, 50, 150)";
-
-for (let i = 0; i < 255; i = i + 1){
-
-  var iFlipped = (i * -1 + 255)
-  var temp = document.createElement('h1')
-  temp.textContent = " a element added--" + String(i);
-  temp.style.color = "hsl("+ i +", 255, 100)"
-  temp.style.backgroundColor = "hsl(" + (i * -1 + 255) + ", 100%, 50%)"
-}
-
-bd.appendChild(temp);
-
-var grabbed = document.getElementById("a");
-grabbed.style.backgroundcolor = "rgb(255, 0, 255)";
-grabbed.style.color = "rgb(0, 50, 150)"
+//
+//
+// console.log("I'm here");
+//
+// var bd = document.body;
+// bd.style.backgroundColor = "White";
+//
+//
+//
+// var headerElement=document.createElement('h1')
+// headerElement.textContent="404 PAGE NOT FOUND";
+// bd.appendChild(headerElement);
+// headerElement.style.color = "rgb(0, 255, 75)";
+//
+// var grabbed = document.getElementById("a");
+// grabbed.style.color = "rgb(255, 0, 255)";
+// grabbed.style.color="rgb(0, 50, 150)";
+//
+// for (let i = 0; i < 255; i = i + 1){
+//
+//   var iFlipped = (i * -1 + 255)
+//   var temp = document.createElement('h1')
+//   temp.textContent = " a element added--" + String(i);
+//   temp.style.color = "hsl("+ i +", 255, 100)"
+//   temp.style.backgroundColor = "hsl(" + (i * -1 + 255) + ", 100%, 50%)"
+// }
+//
+// bd.appendChild(temp);
+//
+// var grabbed = document.getElementById("a");
+// grabbed.style.backgroundcolor = "rgb(255, 0, 255)";
+// grabbed.style.color = "rgb(0, 50, 150)"
